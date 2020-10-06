0ebfea07b18126e33923f7ecbddcfcb986ba33b1
SapnaDorbi <savydorbi@gmail.com>
2020-10-07 00:39:50 +0530

    added toolbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Toolbar.module.css';
+
+const toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        <div>Menu</div>
+    </header>
+);
+
+export default toolbar;
