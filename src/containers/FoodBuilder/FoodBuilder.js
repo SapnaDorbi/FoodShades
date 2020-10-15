a9f1e273f2b391fd4973b0006f457fe2b1e24a72
SapnaDorbi <savydorbi@gmail.com>
2020-10-16 00:13:41 +0530

    navigating to the checkout page

--- src/containers/FoodBuilder/FoodBuilder.js
+++ src/containers/FoodBuilder/FoodBuilder.js
@@ -26,6 +26,7 @@ class FoodBuilder extends Component {
     }
     
     componentDidMount () {
+        // console.log(this.props, "dev"); 
         axios.get('https://foodshades-f7038.firebaseio.com/ingredients.json')
         .then(response => {
             this.setState({ingredients: response.data});
@@ -87,28 +88,30 @@ class FoodBuilder extends Component {
     
     purchaseContinueHandler = () => {
         // alert('You Continue!');
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Sapna Dorbi',
-                address: {
-                    steet: 'Half moon line',
-                    zipCode: '98765',
-                    country: 'India'
-                },
-                email: 'test@gmail.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json',order)
-        .then(response =>{
-            this.setState({ loading: false, purchasing: false });
-        }) 
-        .catch(error => {
-            this.setState({ loading: false, purchasing: false });
-        });
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Sapna Dorbi',
+        //         address: {
+        //             steet: 'Half moon line',
+        //             zipCode: '98765',
+        //             country: 'India'
+        //         },
+        //         email: 'test@gmail.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json',order)
+        // .then(response =>{
+        //     this.setState({ loading: false, purchasing: false });
+        // }) 
+        // .catch(error => {
+        //     this.setState({ loading: false, purchasing: false });
+        // });
+
+        this.props.history.push('/checkout');
     }
     
     render() {
